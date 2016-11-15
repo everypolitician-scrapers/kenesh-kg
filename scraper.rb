f4fb5d31af5d96fe7406cbdd62a49d1abc2faaef
Tony Bowden <tony@tmtm.com>
2016-11-15 12:03:05 +0000

    Turn on Archiving

--- scraper.rb
+++ scraper.rb
@@ -5,7 +5,8 @@ require 'require_all'
 require_rel 'lib'
 
 listpage = 'http://www.kenesh.kg/ru/deputy/list/35'
-strategy = ScrapedPage::Strategy::LiveRequest.new
+strategy = ScrapedPage::Strategy::LiveRequestArchive.new
+# strategy = ScrapedPage::Strategy::LiveRequest.new
 
 page = MembersListPage.new(url: listpage, strategy: strategy)
 warn page.to_h
