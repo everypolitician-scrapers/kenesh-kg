d0c44e1cf1a0db98be9025df0fdc36d895d4d29c
Tony Bowden <tony@tmtm.com>
2016-11-15 12:17:58 +0000

    Add `members` field
    Return a list of all the Member data available on the list page.
    
    This should really live in a fragment class where each of these things
    are fields.

--- lib/members_list_page.rb
+++ lib/members_list_page.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 require 'scraped_page'
+require 'pry'
 
 class MembersListPage < ScrapedPage
   field :term_id do
     noko.css('h4.news-title').text[/(\w+) созыва/, 1]
+  end
+
+  field :members do
+    # TODO: extract to a Fragment class
+    noko.xpath('//table[@class="table"]//tr[td]').map do |tr|
+      td = tr.css('td')
+      {
+        number:  td[0].text,
+        name:    td[1].text,
+        url:     td[1].css('a/@href').text,
+        faction: td[2].text,
+      }
+    end
   end
 end
