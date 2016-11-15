cc7bd439d737c8bcb41c2c0b0cddc6f3f94c35a0
Tony Bowden <tony@tmtm.com>
2016-11-15 12:19:13 +0000

    Temporarily switch to open-uri-cached approach
    No need to keep writing to the archive whilst in develoipment.

--- scraper.rb
+++ scraper.rb
@@ -5,8 +5,9 @@ require 'require_all'
 require_rel 'lib'
 
 listpage = 'http://www.kenesh.kg/ru/deputy/list/35'
-strategy = ScrapedPage::Strategy::LiveRequestArchive.new
-# strategy = ScrapedPage::Strategy::LiveRequest.new
+# strategy = ScrapedPage::Strategy::LiveRequestArchive.new
+strategy = ScrapedPage::Strategy::LiveRequest.new
+require 'open-uri/cached'
 
 page = MembersListPage.new(url: listpage, strategy: strategy)
 warn page.to_h
